89403e96e82ce87a0d41944e607d207eb9b345c6
Pierce Lally <pierce@vulpes.dev>
2025-02-14 21:44:43 -0500

    Update cfc.lua

--- lua/cfc_http_restrictions/configs/server/cfc.lua
+++ lua/cfc_http_restrictions/configs/server/cfc.lua
@@ -3,5 +3,6 @@ return {
 
   addresses = {
     ["cf-steam-api.redoxdev.workers.dev"] = { allowed = true, permanent = true },
+    ["gm-http-proxy.fly.dev"] = { allowed = true, permanent = true },
   }
 }
